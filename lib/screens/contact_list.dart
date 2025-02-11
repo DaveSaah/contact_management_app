59a6ddc136f957d2b869c7e0e4ef302257764387
David Saah <davesaah@gmail.com>
2025-02-11 12:53:54 +0000

    modularise app

--- lib/screens/contact_list.dart
+++ lib/screens/contact_list.dart
@@ -1,20 +1,6 @@
 import 'package:contact_management_app/models/models.dart';
+import 'package:contact_management_app/services/service.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-
-Future<ContactList> fetchAllContacts() async {
-  final response = await http.get(Uri.parse(
-    'https://apps.ashesi.edu.gh/contactmgt/actions/get_all_contact_mob',
-  ));
-
-  if (response.statusCode == 200) {
-    List<dynamic> jsonData = jsonDecode(response.body);
-    return ContactList.fromJson(jsonData);
-  } else {
-    throw Exception('Failed to load contacts');
-  }
-}
 
 class ContactListScreen extends StatefulWidget {
   const ContactListScreen({super.key});
@@ -24,6 +10,17 @@ class ContactListScreen extends StatefulWidget {
 }
 
 class _ContactListScreen extends State<ContactListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return ContactsListView();
+  }
+}
+
+class ContactsListView extends StatelessWidget {
+  const ContactsListView({
+    super.key,
+  });
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<ContactList>(
@@ -38,19 +35,33 @@ class _ContactListScreen extends State<ContactListScreen> {
         }
 
         List<Contact> contacts = snapshot.data!.contacts;
-        return ListView.builder(
-          itemCount: contacts.length,
-          itemBuilder: (context, index) {
-            final contact = contacts[index];
+        return ContactListItems(contacts: contacts);
+      },
+    );
+  }
+}
 
-            return ListTile(
-              leading: CircleAvatar(
-                child: Text(contact.pname[0]),
-              ),
-              title: Text(contact.pname),
-              subtitle: Text(contact.pphone),
-            );
-          },
+class ContactListItems extends StatelessWidget {
+  const ContactListItems({
+    super.key,
+    required this.contacts,
+  });
+
+  final List<Contact> contacts;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: contacts.length,
+      itemBuilder: (context, index) {
+        final contact = contacts[index];
+
+        return ListTile(
+          leading: CircleAvatar(
+            child: Text(contact.pname[0]),
+          ),
+          title: Text(contact.pname),
+          subtitle: Text(contact.pphone),
         );
       },
     );
--- lib/services/service.dart
+++ lib/services/service.dart
@@ -1,2 +1,16 @@
+import 'package:contact_management_app/models/models.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
+Future<ContactList> fetchAllContacts() async {
+  final response = await http.get(Uri.parse(
+    'https://apps.ashesi.edu.gh/contactmgt/actions/get_all_contact_mob',
+  ));
 
+  if (response.statusCode == 200) {
+    List<dynamic> jsonData = jsonDecode(response.body);
+    return ContactList.fromJson(jsonData);
+  } else {
+    throw Exception('Failed to load contacts');
+  }
+}
