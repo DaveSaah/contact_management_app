795bbcd3267e8901a7868184f8353d81d76b2de5
David Saah <davesaah@gmail.com>
2025-02-11 21:38:00 +0000

    del: fetch single contact not used

--- lib/services/service.dart
+++ lib/services/service.dart
@@ -2,18 +2,6 @@ import 'package:contact_management_app/models/models.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-Future<Contact> fetchContact() async {
-  final response = await http.get(Uri.parse(
-    'https://apps.ashesi.edu.gh/contactmgt/actions/get_a_contact_mob?contid=6',
-  ));
-
-  if (response.statusCode == 200) {
-    return Contact.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
-  } else {
-    throw Exception('Failed to load contact');
-  }
-}
-
 Future<ContactList> fetchAllContacts() async {
   final response = await http.get(Uri.parse(
     'https://apps.ashesi.edu.gh/contactmgt/actions/get_all_contact_mob',
